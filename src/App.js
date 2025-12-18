d36a50b3208d8b3517c9acdbcbf1df9cce1625f2
“abisharan-07” <kunam.abisharan07@gmail.com>
2025-12-18 14:49:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import Faq from "./pages/Faq";
+import Contact from "./pages/Contact";
+import "./assets/css/animate.css";
+import "./assets/css/flex-slider.css";
+import "./assets/css/fontawesome.css";
+import "./assets/css/owl.css";
+import "./assets/css/templatemo-tale-seo-agency.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/"  element={<Home/>} />
+        <Route path="/faqs"  element={<Faq/>} />
+        <Route path="/about_us"  element={<About/>} />
+        <Route path="/contact"  element={<Contact/>} />
+      </Routes>
+    </>
   );
 }
 
